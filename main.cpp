5373d6d37e6cb1e81ead7df8c10540522738524e
Akka3223 <58777333+Akka3223@users.noreply.github.com>
2024-02-07 14:58:47 +0200

    Added crypto_hash native

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <stdlib.h>
 #include <string>
 #include <stdio.h>
+#include <algorithm>
+#include <cctype>
+#include <string>
 
 #include "main.h"
 #include "amxxmodule.h"
 
 #include <bcrypt.h>
+#include "crypto/md5.hpp"
+#include "crypto/sha1.hpp"
+#include "crypto/sha256.hpp"
+#include "crypto/sha3.hpp"
+#include "crypto/crc32.hpp"
+#include "crypto/keccak.hpp"
+#include "crypto/base64.hpp"
 
 int main()
 {
@@ -29,10 +39,115 @@ static cell AMX_NATIVE_CALL bcrypt_validate( AMX *amx, cell *params)
 
     return newpass;
 }
+static cell AMX_NATIVE_CALL Crypto_Hash(AMX* amx, cell* params) {
+	enum args {
+		arg_count,
+		arg_hashType,
+		arg_string,
+		arg_result
+	};
+
+	int stringLength(0), hashTypeLength(0);
+
+	auto hashType 	= MF_GetAmxString(amx, params[arg_hashType], 0, nullptr);
+	auto string 	= MF_GetAmxString(amx, params[arg_string], 1, nullptr);
+
+	std::string checkHashType(hashType);
+
+	/*! Transform to case-insensitive for comparison */
+	std::transform(checkHashType.begin(), checkHashType.end(), checkHashType.begin(), ::tolower);
+
+	/*! Compare if we have found the algorithm */
+	if (checkHashType == "md5") {
+		MD5 hash;
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+		
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	}
+	else if (checkHashType == "sha1") {
+		SHA1 hash;
+		std::string iStringifiedText(string);
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	} else if (checkHashType == "sha256") {
+		SHA256 hash;
+		std::string iStringifiedText(string);
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	} else if (checkHashType == "sha3") {
+		SHA3 hash;
+		std::string iStringifiedText(string);
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	} else if (checkHashType == "crc32") {
+		CRC32 hash;
+		std::string iStringifiedText(string);
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	}
+	else if (checkHashType == "keccak") {
+		Keccak hash;
+		std::string iStringifiedText(string);
+
+		auto hashedString = hash(std::string(string));
+		char* test = &hashedString[0];
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	}
+	else if (checkHashType == "base64") {
+		BASE64 hash;
+		std::string iStringifiedText(string);
+		char ret[2048] = "\0";
+		int len;
+		hash.Encode(std::string(string).c_str(), std::string(string).size(), ret, &len);
+		std::string hashedString(ret);
+
+		MF_SetAmxString(amx, params[arg_result], hashedString.c_str(), hashedString.length());
+
+		return 0;
+	} else {
+		std::string errMsg = "[" + std::string(MODULE_NAME) + "] Hash type '" + checkHashType + "' is not supported!\n";
+		char* error = &errMsg[0];
+
+		MF_PrintSrvConsole(error);
+
+		return 1;
+	}
+	
+	return -1;
+}
+
 AMX_NATIVE_INFO bcrypt_natives[] = 
 {
     { "bcrypt", bcrypt_hash },
     { "bcrypt_validate", bcrypt_validate },
+    { "crypto_hash", Crypto_Hash },
     { NULL, NULL }
 };
 void OnAmxxAttach()
